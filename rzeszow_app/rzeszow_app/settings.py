68713fa9c71b7026084e1d49b9c65f0113458720
LAICEROO <mati151010@gmail.com>
2023-10-27 14:04:42 +0200

    Added required star to form

--- rzeszow_app/rzeszow_app/settings.py
+++ rzeszow_app/rzeszow_app/settings.py
@@ -68,7 +68,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'mylifeclinicdb',
         'USER': 'root',
-        'PASSWORD': 'HZJ8D9DBfUAZeYK8PLh1',
+        'PASSWORD': 'xRMajsha942mgCQrCjwH',
         'HOST': 'localhost',
         'PORT': '3306',
     }
