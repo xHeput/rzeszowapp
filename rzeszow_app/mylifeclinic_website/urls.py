2bd180e2e8b1e664fd5e156e68d344ec83591e07
Steksu <pawellstrzepkaa@gmail.com>
2023-11-11 12:24:45 +0100

    Implemented login and logout for user

--- rzeszow_app/mylifeclinic_website/urls.py
+++ rzeszow_app/mylifeclinic_website/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.home, name = 'home'),
     path('form/', views.register_user, name = 'form'),
     path('login/', views.login_user, name = 'login'),
+    path('logout/', views.logout_user, name = 'logout'),
     path('statute/',views.statute, name='statute'),
     path('panel;/',views.panel_user, name='panel'),
 ]
--- rzeszow_app/mylifeclinic_website/views.py
+++ rzeszow_app/mylifeclinic_website/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def home(request):
     return render(request, 'home.html', {})
@@ -7,7 +9,26 @@ def register_user(request):
     return render(request, 'form.html', {})
 
 def login_user(request):
-    return render(request, 'login.html', {})
+    # Chack to see if logging in
+    if request.method == 'POST':
+        username = request.POST['emaile']
+        password = request.POST['password']
+        # Authenticate
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Zalogowano pomyślnie!")
+            return redirect('panel')
+        else:
+            messages.success(request, "Wystąpił błąd podczas logowania, spróbuj ponownie...")
+            return redirect('login')
+    else:
+        return render(request, 'login.html', {})
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "Wylogowano pomyślnie...")
+    return redirect('home')
 
 def statute(request):
     return render(request,'statute.html',{})
