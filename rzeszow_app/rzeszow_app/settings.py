ef11711a8c0ec85b072a45d9078b3e20bfd7cd13
LAICEROO <mati151010@gmail.com>
2023-11-13 16:30:16 +0100

    panel update

--- rzeszow_app/rzeszow_app/settings.py
+++ rzeszow_app/rzeszow_app/settings.py
@@ -66,9 +66,9 @@ WSGI_APPLICATION = 'rzeszow_app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mylife_clinic',
+        'NAME': 'mylifeclinicdb',
         'USER': 'root',
-        'PASSWORD': 'HZJ8D9DBfUAZeYK8PLh1',
+        'PASSWORD': 'xRMajsha942mgCQrCjwH',
         'HOST': 'localhost',
         'PORT': '3306',
     }
